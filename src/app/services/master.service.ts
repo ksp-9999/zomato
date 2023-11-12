fdfcdc80700704d158e3dcab908b6f76a8e58a3d
ksp <kilarusaiprasad19@gmail.com>
2023-11-12 10:46:53 +0530

    files added

---
+++ src/app/services/master.service.ts
@@ -0,0 +1,262 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MasterService {
+
+  constructor(private http:HttpClient) { 
+  }
+  getAllFoodItems(){
+    return this.http.get('https://freeapi.miniprojectideas.com/api/zomato/GetAllFoodCategory')
+  }
+restaurants={
+hyderabad:[
+    {
+        name:'Hotel Nigara since-1960',
+        location:'chanderghat,Hyderabad',
+        rating:4,
+        img:'../../../assets/Biryanis.png',
+        Menu:{
+            biryanis:[
+                {
+                    name:'Niagara Zafrani Mutton Biryani',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\nigarazafranimuttonbiryani.webp'
+                },
+                {
+                    name:'Chicken Biryani Single',
+                    price:220,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\chickenbiryanisingle.webp'
+                },
+                {
+                    name:'Chicken Kheema Biryani',
+                    price:320,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\chickenkeemabiryai.webp'
+                },
+                {
+                    name:'Chicken Special Biryani (double Piece)',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\chickensplbiryanidoublepiece.webp'
+                },
+                {
+                    name:'Prawns Biryani (special)',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\prawnsbiryanispl.webp'
+                },
+                {
+                    name:'Chicken 65 Biryani',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\chicken65biryani.webp'
+                },
+                {
+                    name:'Chicken Biryani',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\chickenbiryani.webp'
+                },
+                {
+                    name:'Mutton Biryani',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\muttonbiryani.webp'
+                },
+                {
+                    name:'Fish Biryani',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\fishbiryani.webp'
+                },
+                {
+                    name:'Chicken Handi Biryani [serves 2]',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\chickenhandibiryaniserves2.webp'
+                },
+                {
+                    name:'Mutton Double Gosht Biryani',
+                    price:368,
+                    rating:4,
+                    img:''
+                },
+                {
+                    name:'Mutton Keema Biryani',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\muttondoubleghostbiryani.webp'
+                },
+                {
+                    name:'Red Chicken With 2 Rumali Roti',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\redchickenwith2rumaliroti.webp'
+                },
+                {
+                    name:'Chicken Masala 2 Tandoori Roti',
+                    price:368,
+                    rating:4,
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\Biryanis\chickenmasala2thandooriroti.webp'
+                },
+            ],
+            breakfast:[
+                {
+                    name:'Boneless Chicken Mughlai',
+                    price:'',
+                    rating:'',
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\chickenmaincourse\bonelesschickenMughulai.webp'
+                },
+                {
+                    name:'Achari Chicken Boneless',
+                    price:'',
+                    rating:'',
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\chickenmaincourse\acharichickenboneless.webp'
+                },
+                {
+                    name:'Kadai Chicken Boneless',
+                    price:'',
+                    rating:'',
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\chickenmaincourse\kadaichickenboneless.webp'
+                },
+                {
+                    name:'Chicken Shahi Korma Boneless',
+                    price:'',
+                    rating:'',
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\chickenmaincourse\chickenshahikormaBL.webp'
+                },
+                {
+                    name:'Methi Chicken Boneless',
+                    price:'',
+                    rating:'',
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\chickenmaincourse\methichickenBL.webp'
+                },
+                {
+                    name:'Chicken Masala',
+                    price:'',
+                    rating:'',
+                    img:'C:\Users\DELL\OneDrive\Desktop\Angular_Projects\ZomatoProject\src\assets\HotelNigara\chickenmaincourse\chickenmasala.webp'
+                },
+                {
+                    name:'Dawat Wala Red Chicken',
+                    price:'',
+                    rating:'',
+                    img:''
+                },
+                {
+                    name:'Andhra Chicken Curry Boneless',
+                    price:'',
+                    rating:'',
+                    img:''
+                },
+                {
+                    name:'Boneless Butter Chicken',
+                    price:'',
+                    rating:'',
+                    img:''
+                },
+                {
+                    name:'Boneless Chicken Jalfrezi',
+                    price:'',
+                    rating:'',
+                    img:''
+                },
+                {
+                    name:'Boneless Chicken Tikka Masala',
+                    price:'',
+                    rating:'',
+                    img:''
+                },
+                {
+                    name:'Boneless Ginger Chicken',
+                    price:'',
+                    rating:'',
+                    img:''
+                },
+            ],
+            breads:[],
+            meal:[],
+            non_veg_starters:[],
+            chicken_main_course:[],
+            mutton_main_course:[],
+            fried_rice:[],
+            deserts:[],
+            soups:[],
+            backery:[],
+            platter:[],
+            nigara_spl:[]
+        }
+    },
+    {
+        name:'Udipi\'s Upahar',
+        location:'Jubilee Hills,Hyderabad',
+        rating:'4.2',
+        img:"../../../assets/Dosa_tiffins.jpg",
+        menu:{}
+    },
+    {
+        name:'Varalakshmi Tiffins',
+        location:'Banjara Hills,Hyderabad',
+        rating:'4.3',
+        img:"../../../assets/restaurants/hyd/varalakshmitiffin/resimg.webp",
+        menu:{}
+    },
+    {
+        name:'Theobroma',
+        location:'Filmnagar,Hyderabad',
+        rating:'4.4',
+        img:"../../../assets/restaurants/hyd/Theobroma/resimg.webp",
+        menu:{}
+    },
+    {
+        name:'Burger King',
+        location:'Tolichowki,Hyderabad',
+        rating:'4.1',
+        img:"../../../assets/restaurants/hyd/burgerking/resimg.webp",
+        menu:{}
+    },
+    {
+        name:'Chutneys',
+        location:'jubilee Hills',
+        rating:'3.8',
+        img:"../../../assets/restaurants/hyd/chutneys/resimg.webp",
+        menu:{}
+    },
+    {
+        name:'Panchakattu Dosa',
+        location:'Hitechcity,Hyderabad',
+        rating:'4.3',
+        img:"../../../assets/restaurants/hyd/panchakattudosa/resimg.webp",
+        menu:{}
+    },
+    {
+        name:'Good Vibes Only Cafe',
+        location:'Gachibowli',
+        rating:'4.2',
+        img:"../../../assets/restaurants/hyd/goodvibesonlycafe/resimg.webp",
+        menu:{}
+    },
+    {
+        name:'Sri Raghavendra',
+        location:'mega hills',
+        rating:'3.5',
+        img:"../../../assets/restaurants/hyd/sriragavendra/resimg.webp",
+        menu:{}
+    },
+    {
+        name:'Gottipati Military Hotel 24 X7',
+        location:'Madhapur',
+        rating:'4.6',
+        img:"../../../assets/restaurants/hyd/gottipati/resimg.webp",
+        menu:{}
+    },
+],
+vijayawada:[],
+Guntur:[]
+}
+}
